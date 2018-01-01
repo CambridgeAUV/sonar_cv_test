4c595052f6e6dcdce562ccb37155422c4526ee82
yl573 <yl573@cam.ac.uk>
2018-01-01 17:25:59 +0800

    added filters

--- script.py
+++ script.py
@@ -25,66 +25,98 @@ def get_grouped_lines(lines):
         ret, label, grouped_lines = cv2.kmeans(lines, 3,None,  criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
         return grouped_lines
     else:
-        print "There were no lines found"
+        print("There were no lines found")
         return []
+
+def filter(img):
+
+    kernel = np.ones((3,3),np.float32)/9
+    img = cv2.filter2D(img,-1,kernel)
+
+    img = cv2.Canny(img,0,80)
+
+    # _, img = cv2.threshold(img, 30, 255, cv2.THRESH_BINARY)
+
+    # convert to skeleton
+    element = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+    skel = np.zeros(img.shape).astype('uint8')
+    
+    while True:
+
+        temp = cv2.morphologyEx(img, cv2.MORPH_OPEN, element)
+        temp = cv2.bitwise_not(temp)
+        temp = cv2.bitwise_and(img, temp)
+        skel = cv2.bitwise_or(skel, temp)
+        img = cv2.erode(img, element)
+        
+        if np.sum(img) == 0:
+            break
+    img = skel
+
+    return img
 
 cap = cv2.VideoCapture('./sonar_vid.avi')
 
 while(cap.isOpened()):
 
     ret, raw_img = cap.read()
+    if raw_img.any():
+        cv2.imshow('raw', raw_img)
 
+    # cut off top 30px, which is text
+    cut_px = 30
+    raw_img = raw_img[cut_px:, :]
+
+    img = np.array(raw_img)
     # Threshold the image
-    gray_img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY)
-    ret, thresh_img = cv2.threshold(gray_img, 30, 255, cv2.THRESH_BINARY)
-
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = filter(img)
 
     # Find Hough lines
-    
-    lines = cv2.HoughLines(thresh_img, 1, np.pi/180, threshold = 30)
+    lines = cv2.HoughLines(img, 1, np.pi/180, threshold = 40)
 
     # Draw the submarine on the image
-    sub_pos = (int(len(raw_img[0])/2), len(raw_img) - 20)
-    cv2.circle(raw_img, sub_pos, 5, (100, 255, 0), -1)
-
-
+    # sub_pos = (int(len(raw_img[0])/2), len(raw_img) - 20)
+    # cv2.circle(raw_img, sub_pos, 5, (100, 255, 0), -1)
 
     # Draw the lines on the original image and find the distance to the lines from the submarine
 
-    dis_to_lines = []
-    text_pos_counter = 0
+    # dis_to_lines = []
+    # text_pos_counter = 0
+    if lines is not None:
+        for line in lines:
+            line = line[0]
+            theta = line[1]
+            rho = line[0]
+            # print("theta: ", theta, " rho: ", rho)
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            length = 2000
+            x1 = int(x0 + length*(-b))
+            y1 = int(y0 + length*(a))
+            x2 = int(x0 - length*(-b))
+            y2 = int(y0 - length*(a))
 
-    for line in lines:
-        line = line[0]
-        theta = line[1]
-        rho = line[0]
-        print "theta: ", theta, " rho: ", rho
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        length = 2000
-        x1 = int(x0 + length*(-b))
-        y1 = int(y0 + length*(a))
-        x2 = int(x0 - length*(-b))
-        y2 = int(y0 - length*(a))
+            # x1, y1, x2, y2 = tuple(line[0])
+            cv2.line(raw_img, (x1, y1), (x2, y2), (0, 0, 255), 1)
 
-        cv2.line(raw_img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-        dis_to_line = get_dis_to_line([sub_pos[0], sub_pos[1]], [x1, y1], [x2, y2], raw_img)
-        print "dis_to_line: ", dis_to_line
-        # cv2.putText(raw_img, "Line " + str(text_pos_counter) + " dis: " + str(dis_to_line), (100 , 200 + 100 * text_pos_counter), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
-        text_pos_counter += 1
+            # dis_to_line = get_dis_to_line([sub_pos[0], sub_pos[1]], [x1, y1], [x2, y2], raw_img)
+            # print("dis_to_line: ", dis_to_line)
+            # # cv2.putText(raw_img, "Line " + str(text_pos_counter) + " dis: " + str(dis_to_line), (100 , 200 + 100 * text_pos_counter), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
+            # text_pos_counter += 1
 
 
 
     # cv2.imshow('video',cv2.resize(raw_img, ( int(len(raw_img[0]) * 0.5), int(len(raw_img) * 0.5)) ))
+    cv2.imshow('filtered', img)
     cv2.imshow('video', raw_img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        print "Breaking loop"
+        print("Breaking loop")
         break
 cap.release()
 cv2.destroyAllWindows()
 
 
-print "End of program"
+print("End of program")
